a39605bcf09e9673d363c4866ee3a2b073a74129
MatheusFreitag <freitag.matheus@gmail.com>
2019-10-15 07:42:40 -0300

    TF-IDF

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ most_popular_words_inView = false;
 word_count_inView = false;
 most_popular_words_melodrama_inView = false;
 most_popular_words_pureHeroine_inView = false;
+TFIDF_melodrama_inView = false;
+TFIDF_pureHeroine_inView = false;
 
 $.fn.isOnScreen = function(){
 
@@ -41,6 +43,16 @@ window.addEventListener("scroll", function(){
     if ($('#chart-most-popular-words-pureHeroine').isOnScreen() && most_popular_words_pureHeroine_inView == false) {
         most_popular_words_pureHeroine_inView = true;
         showMostPopularWords_pureHeroine();
+    }
+
+    if ($('#chart-TFIDF-melodrama').isOnScreen() && TFIDF_melodrama_inView == false) {
+        TFIDF_melodrama_inView = true;
+        showTFIDF_Melodrama();
+    }
+
+    if ($('#chart-TFIDF-pureHeroine').isOnScreen() && TFIDF_pureHeroine_inView == false) {
+        TFIDF_pureHeroine_inView = true;
+        showTFIDF_pureHeroine();
     }
 
 })
@@ -273,6 +285,121 @@ function showMostPopularWords_pureHeroine(){
                     scaleLabel: {
                         display: true,
                         labelString: 'Palavra analisada',
+                        fontColor: 'white'
+                    },
+                    ticks: {
+                        fontColor: 'white'
+                    }
+                }]
+            }
+        }
+    });
+}
+
+
+//-----------------
+
+function showTFIDF_Melodrama(){
+    var ctx_TFIDF_melodrama = document.getElementById("chart-TFIDF-melodrama").getContext('2d');
+    var dataValues_TFIDF_melodrama = [0.9189486384538972,0.7616438130090609,0.6244130909523604,0.609207891745432,0.5976496055935542,0.5558858925588193,0.5372648489806265,0.5278497949693597,0.5207660297584914,0.5152550086940663];
+    var dataLabels_TFIDF_melodrama = ['boom','much','liability','love','supercut','when','places','perfect','thought','make'];
+    var TFIDF_Chart_melodrama = new Chart(ctx_TFIDF_melodrama, {
+    type: 'horizontalBar',
+    maintainAspectRatio: false,
+        data: {
+        labels: dataLabels_TFIDF_melodrama,
+        datasets: [{
+            label: 'Contagem',
+            data: dataValues_TFIDF_melodrama,
+            backgroundColor: 'rgba(255, 255, 255, 1)',
+        }]
+        },
+        options: {
+          aspectRatio: 1,
+            title: {
+                display: true,
+                text: "Palavras mais importantes em Melodrama",
+                fontColor: 'white'
+            },
+            legend: {
+                labels: {
+                fontColor: 'white'
+                }
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    ticks: {
+                        fontColor: 'white',
+                        beginAtZero: true
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Score TF-IDF',
+                        fontColor: 'white'
+                    },
+                }],
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Palavra',
+                        fontColor: 'white'
+                    },
+                    ticks: {
+                        fontColor: 'white'
+                    }
+                }]
+            }
+        }
+    });
+}
+
+
+
+function showTFIDF_pureHeroine(){
+    var ctx_TFIDF_pureHeroine = document.getElementById("chart-TFIDF-pureHeroine").getContext('2d');
+    var dataValues_TFIDF_pureHeroine = [1.457891088352351,1.0269913356705658,0.8538257239238773,0.8271524467621539,0.7897352373440186,0.7563868401339567,0.7010520347122493,0.6979362836474323,0.6851946319552147,0.6792457039939788];
+    var dataLabels_TFIDF_pureHeroine = ['like','yeah','love','people','club','take','call','know','never','talking'];
+    var TFIDF_Chart_pureHeroine = new Chart(ctx_TFIDF_pureHeroine, {
+    type: 'horizontalBar',
+    maintainAspectRatio: false,
+        data: {
+        labels: dataLabels_TFIDF_pureHeroine,
+        datasets: [{
+            label: 'Contagem',
+            data: dataValues_TFIDF_pureHeroine,
+            backgroundColor: 'rgba(255, 255, 255, 1)',
+        }]
+        },
+        options: {
+          aspectRatio: 1,
+            title: {
+                display: true,
+                text: "Palavras mais importantes em Melodrama",
+                fontColor: 'white'
+            },
+            legend: {
+                labels: {
+                fontColor: 'white'
+                }
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    ticks: {
+                        fontColor: 'white',
+                        beginAtZero: true
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Score TF-IDF',
+                        fontColor: 'white'
+                    },
+                }],
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Palavra',
                         fontColor: 'white'
                     },
                     ticks: {
